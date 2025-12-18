a5724d0f59615b40d20a907528af064e07bc5cc5
Ivan Barabanov <barabanov_vanechka06@mail.ru>
2025-12-18 18:11:30 +0300

    add ask number of photos

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ import user_ask as ua
 
 
 def main() -> None:
+    print('Сколько изображений нужно скачать в каждом периоде? ')
+    n = ua.ask_natural()
     periods = ua.ask_info()
     for i in range(len(periods)):
         google_crawler = GoogleImageCrawler(
             storage={'root_dir': 'results/'+str(i)},
             api_key='ВАШ_API_КЛЮЧ',
             cse_id='32d119033722e4c0d'
-            )
+        )
         filters = dict(date=(periods[i].first_date, periods[i].second_date))
-        google_crawler.crawl(keyword='bear', filters=filters)
+        google_crawler.crawl(keyword='bear', filters=filters,
+                             max_num=(n/len(periods)))
 
 
 if __name__ == "__main__":
