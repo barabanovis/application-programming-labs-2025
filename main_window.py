c8321560730e4adb8452afc073afe502d5917a72
Ivan Barabanov <barabanov_vanechka06@mail.ru>
2025-12-20 10:01:12 +0300

    dsf

--- main_window.py
+++ main_window.py
@@ -1,10 +1,27 @@
+import argparse
+
 from PyQt6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QLabel, QWidget
-from PyQt6.QtCore import Qt, QSize
+from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap
+
 from iterator import FileIterator
 
 
+def argument_parsing() -> list[str]:
+    """
+    Разделение аргументов, введённых пользователем в консоли
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('annot_path', type=str, help='path to annotation')
+
+    args = parser.parse_args()
+    return [args.annot_path]
+
+
 class MainWindow(QWidget):
+    def set_annot_path(self, path):
+        self.annot_path = path
+
     def __init__(self):
         self.iter = FileIterator('results.csv')
 
@@ -25,6 +42,7 @@ class MainWindow(QWidget):
     def on_button_click(self):
         try:
             path = self.iter.__next__()
+            print('finding image on path', path)
         except StopIteration:
             self.image_label.setText("You listed all images")
 
@@ -39,9 +57,13 @@ class MainWindow(QWidget):
 
 
 if __name__ == '__main__':
+    args = argument_parsing()
+    annot_path = args[0]
+
     app = QApplication([])
 
     window = MainWindow()
+    window.set_annot_path(annot_path)
     window.show()
 
     app.exec()
