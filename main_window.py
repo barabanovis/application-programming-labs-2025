ccf31fea79daf07d6a7af729089858c5d92c6d2c
Ivan Barabanov <barabanov_vanechka06@mail.ru>
2025-12-20 05:58:48 +0300

    added necessary files

---
+++ main_window.py
@@ -0,0 +1,31 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
+from PyQt6.QtCore import QSize
+from PyQt6 import uic
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My App")
+
+
+        self.label = QLabel()
+        self.button = QPushButton()
+
+        self.button.clicked.connect(self.button_click)
+
+        def button_click(self):
+            
+
+    def button_toggle(self, checked):
+        self.button_cheched = checked
+        print(self.button_cheched)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    window = MainWindow()
+    window.show()
+
+    app.exec()
